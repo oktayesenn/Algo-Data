3938b640f6d5ea558c498f42e6bd2779dd6e31d3
Oktay esen <oktayesenn@gmail.com>
2022-10-10 07:59:00 -0400

    Test

---
+++ algo.py
@@ -0,0 +1,50 @@
+# def bigon(n):
+#     for i in range(0, n):
+#         print(i)
+# bigon(5)
+
+# def bigon2(n):
+#     for i in range (0,n):
+#         for j in range (0,n):
+#             print(i,j)
+# bigon2(5)            
+
+
+# def nfactorial(n):
+#     if n ==0:
+#         print("1")
+#         return
+#     else:
+#         for i in range(0,n):
+#             print(i)
+#             nfactorial(n-1)
+# nfactorial(2)            
+
+# from queue import Queue
+# from typing import Deque
+
+
+# Array, List, Stack , Queue, Deque
+# import sys
+
+# n = 15 
+# myDynamicArray = []
+# for i in range(n):
+#     myLength = len(myDynamicArray)
+#     myByte = sys.getsizeof(myDynamicArray)
+#     print(f"Length: {myLength} Byte: {myByte}")
+#     myDynamicArray.append(n)
+
+
+
+# nums = [1,2,3,4,1,4,2]
+# for i in range (len(nums)):
+#     if i in nums[]:
+#         print("True")
+#     else:
+#         print("False")
+
+
+
+
+
